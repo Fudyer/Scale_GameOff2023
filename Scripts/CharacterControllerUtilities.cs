6b0812a118daa1e3bd0b372e4fafecd0b2a28758
Fudyer <117793195+Fudyer@users.noreply.github.com>
2023-11-03 09:49:52 -0700

    Adding several dialogue files and prefabs

---
+++ Scripts/CharacterControllerUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DiasGames.Components;
+public class CharacterControllerUtilities : MonoBehaviour
+{
+	public ICapsule capsule;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    capsule = GetComponent<ICapsule>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/ParticleFlasher.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFlasher : MonoBehaviour
+{
+	public List<ParticleSystem> particleSystems = new List<ParticleSystem>();
+	public Vector2 delayMinMax;
+	bool triggered = false;
+	float triggerTime = 0f;
+	float timer = 0f;
+	// Update is called every frame, if the MonoBehaviour is enabled.
+	protected void Update()
+	{
+		timer += Time.deltaTime;
+		if(triggered) {
+			triggered = !triggered;
+			timer = 0f;
+			triggerTime = Random.Range(delayMinMax.x,delayMinMax.y);
+			TriggerParticles();
+		} else if(timer > triggerTime) {
+			triggered = true;
+		}
+		
+		
+	}
+	// Function to trigger all the particle systems in the list
+	public void TriggerParticles()
+	{
+		foreach (ParticleSystem ps in particleSystems)
+		{
+			ps.Play();
+		}
+	}
+}
