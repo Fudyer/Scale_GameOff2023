d7553ff03f4e1accd41d2e41f7e3903d6aa45479
Fudyer <117793195+Fudyer@users.noreply.github.com>
2023-11-06 15:41:32 -0800

    Pushing updates to level, several scripts and more dialogue files

---
+++ Scripts/DialogueTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+	public AudioClip clip;
+	public float distanceToTrigger = 1f;
+	public bool triggered = false;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+	
+    // Update is called once per frame
+    void Update()
+	{
+		if(triggered) return;
+		float distance = Vector3.Distance(this.transform.position, GameJefe.Instance.characterTransform.position);
+		if(distance <= distanceToTrigger) {
+			triggered = true;
+			
+			GameJefe.Instance.characterAudioSource.clip = clip;
+			GameJefe.Instance.characterAudioSource.Play();
+		}
+	}
+	// Implement OnDrawGizmos if you want to draw gizmos that are also pickable and always drawn.
+	protected void OnDrawGizmos()
+	{
+		Gizmos.DrawWireSphere(transform.position,distanceToTrigger);
+	}
+}
---
+++ Scripts/GameJefe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameJefe : MonoBehaviour
+{
+	public static GameJefe Instance { get; private set;}
+	public AudioSource characterAudioSource;
+	public Transform characterTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    if(Instance == null) {
+	    	Instance = this;
+	    }        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
